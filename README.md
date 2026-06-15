746bc7747d79938e5a768ef2761ab176b144e9aa
Karolina Guzik <kontakt.ikagi@gmail.com>
2026-06-15 12:11:02 +0200

    chore: init project

---
+++ README.md
@@ -0,0 +1,13 @@
+## Getting Started
+
+First, install the dependencies and run the development server:
+
+```bash
+pnpm install
+```
+
+```bash
+pnpm dev
+```
+
+Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div className='flex flex-col flex-1 items-center justify-center'>
+      <main>
+        <div>
+          <h1 className='text-4xl font-bold'>Vibe Lookbook</h1>
+        </div>
+      </main>
+    </div>
+  );
+}
